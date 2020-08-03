c03ad965ff3e80203991d7cd839138326f96cab3
Russell Anderson <russellanderson2000@gmail.com>
2020-08-03 16:36:19 -0400

    Add project files.

---
+++ FormUI/DataAccess.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+namespace FormUI
+{
+    public class DataAccess
+    {
+        public List<Person> GetPeople(string lastName)
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.ConnVal("SampleDB")))
+            {
+               var output = connection.Query<Person>($"select * from People where LastName= '{lastName}' ").ToList();
+                return output;
+            }
+        }
+    }
+}
---
+++ FormUI/Helper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormUI
+{
+    public static class Helper
+    {
+        public static string ConnVal(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+    }
+}
